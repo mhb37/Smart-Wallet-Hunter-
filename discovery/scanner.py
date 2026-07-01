af64fdcec0d4aaec9fb865a54aa67dc86d737fa7
mhb37 <bryan_37@hotmail.fr>
2026-07-01 10:30:02 +0200

    Create scanner.py

---
+++ discovery/scanner.py
@@ -0,0 +1,13 @@
+def discover_wallet_candidates():
+    """
+    Fonction temporaire.
+    Retournera plus tard des adresses Solana.
+    """
+
+    candidates = [
+        "wallet_1",
+        "wallet_2",
+        "wallet_3"
+    ]
+
+    return candidates

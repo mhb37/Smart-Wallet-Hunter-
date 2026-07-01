5d835294e5adbd4487a54dfacb1e398884f25623
mhb37 <bryan_37@hotmail.fr>
2026-07-01 12:19:42 +0200

    Update graph.py

--- analysis/graph.py
+++ analysis/graph.py
@@ -1,6 +1,29 @@
 from collections import defaultdict
+import json
+import os
+
+GRAPH_FILE = "storage/graph.json"
 
 GRAPH = defaultdict(dict)
+
+
+def load_graph():
+    global GRAPH
+
+    if not os.path.exists(GRAPH_FILE):
+        return
+
+    with open(GRAPH_FILE, "r") as f:
+        data = json.load(f)
+
+    for k, v in data.items():
+        GRAPH[k] = v
+
+
+def save_graph():
+
+    with open(GRAPH_FILE, "w") as f:
+        json.dump(GRAPH, f)
 
 
 def add_connections(wallets):
@@ -11,7 +34,6 @@ def add_connections(wallets):
             if w1 == w2:
                 continue
 
-            # poids du lien (co-appearance)
             if w2 in GRAPH[w1]:
                 GRAPH[w1][w2] += 1
             else:

2f9fd1d268b8f259144edde2206e22f50b7f3214
mhb37 <bryan_37@hotmail.fr>
2026-07-01 21:40:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,44 @@
 import os
 import logging
+
 from telegram.ext import ApplicationBuilder
 
 from telegram_bot.handlers import register_handlers
 from scheduler.jobs import start_scheduler
 
-logging.basicConfig(level=logging.INFO)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(message)s"
+)
 
 
 def main():
+
     token = os.getenv("BOT_TOKEN")
+
     if not token:
         raise ValueError("BOT_TOKEN missing")
 
-    app = ApplicationBuilder().token(token).build()
+    app = (
+        ApplicationBuilder()
+        .token(token)
+        .build()
+    )
 
     register_handlers(app)
 
+    # Supprime un éventuel ancien webhook
+    app.bot.delete_webhook(drop_pending_updates=True)
+
+    # Lance le scanner
     start_scheduler()
 
-    app.run_polling(drop_pending_updates=True)
+    # Telegram polling
+    app.run_polling(
+        drop_pending_updates=True,
+        close_loop=False,
+    )
 
 
 if __name__ == "__main__":

874be3fe61dfb67c8abb0405e3732a6e135d24c9
mhb37 <bryan_37@hotmail.fr>
2026-07-01 10:12:32 +0200

    Update bot.py

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -1,1 +1,23 @@
-1
+from telegram.ext import ApplicationBuilder, CommandHandler
+
+from config import TELEGRAM_TOKEN
+from telegram_bot.commands import (
+    start,
+    status,
+    help_command
+)
+
+
+def create_bot():
+
+    app = (
+        ApplicationBuilder()
+        .token(TELEGRAM_TOKEN)
+        .build()
+    )
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("status", status))
+    app.add_handler(CommandHandler("help", help_command))
+
+    return app

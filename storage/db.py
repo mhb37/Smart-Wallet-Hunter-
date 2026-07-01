1ad376ab3ff1dc65bfd3bb9b3c0b81ad76507908
mhb37 <bryan_37@hotmail.fr>
2026-07-01 16:56:54 +0200

    Update db.py

--- storage/db.py
+++ storage/db.py
@@ -5,6 +5,7 @@ DB_PATH = "storage/wallets.db"
 
 
 def init_db():
+
     conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
 
@@ -22,23 +23,37 @@ def init_db():
 
 
 def upsert_wallet(wallet):
+
     now = int(time.time())
 
     conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
 
-    c.execute("SELECT * FROM wallets WHERE wallet = ?", (wallet,))
+    c.execute(
+        "SELECT * FROM wallets WHERE wallet = ?",
+        (wallet,)
+    )
+
     row = c.fetchone()
 
     if row:
+
         c.execute("""
         UPDATE wallets
-        SET last_seen = ?, appearances = appearances + 1
+        SET last_seen = ?,
+            appearances = appearances + 1
         WHERE wallet = ?
         """, (now, wallet))
+
     else:
+
         c.execute("""
-        INSERT INTO wallets (wallet, first_seen, last_seen, appearances)
+        INSERT INTO wallets (
+            wallet,
+            first_seen,
+            last_seen,
+            appearances
+        )
         VALUES (?, ?, ?, 1)
         """, (wallet, now, now))
 
@@ -47,11 +62,20 @@ def upsert_wallet(wallet):
 
 
 def get_all_wallets():
+
     conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
 
-    c.execute("SELECT * FROM wallets")
+    c.execute("""
+    SELECT wallet,
+           first_seen,
+           last_seen,
+           appearances
+    FROM wallets
+    """)
+
     rows = c.fetchall()
 
     conn.close()
+
     return rows

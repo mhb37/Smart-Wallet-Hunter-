a4622e6f1c13a48c1bc8faad60ebe5615e06131e
mhb37 <bryan_37@hotmail.fr>
2026-07-01 10:12:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-1
+from telegram_bot.bot import create_bot
+
+
+def main():
+
+    bot = create_bot()
+
+    print("🚀 Smart Wallet Hunter lancé")
+
+    bot.run_polling()
+
+
+if __name__ == "__main__":
+    main()
